1fb9da3aa1aca09a9af8192e1ec0316370837fcd
Okeke Chukwuebuka <chukwuebukaokeke09@gmail.com>
2026-09-18 10:51:47 +0100

    Updated scroll to top

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,60 +1,21 @@
-$('[data-toggle="collapsible-nav"]').on('click', function(e){
-    var target = ($(this).data('target'));
-    $('#' + target).toggleClass('show');
-});
+(function ($) {
+    ("use strict");
 
-$(document).ready(function(){
-    if(window.innerWidth >= 992) {
-        $('#collapsible-nav').addClass('show');  //Show navigation menu in bigger screens by default.
+    $(".back-to-top").fadeOut();
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
     } else {
-        $('#collapsible-nav').removeClass('show');
+      $(".back-to-top").fadeOut("slow");
     }
-
-    if ($('.hover-box').length) {
-        setHoverBoxPerspective();
-    }
-});
-
-$(window).resize(
-    function() {
-        if ($('.hover-box').length) {
-            setHoverBoxPerspective();
-        }
-    }
-);
-
-function setHoverBoxPerspective() {
-    $('.hover-box').css({
-        'perspective': function () {
-            return Math.max( $(this).width(), $(this).height() ) * 2 + 50;
-        }
-    });
-}
-
-
-var classNames = ['in-up', 'in-right', 'in-down', 'in-left', 'out-up', 'out-right', 'out-down', 'out-left']; // Animation classes.
-
-$('.hover-box').hover(
-    function (event) {
-        var direction = "up";
-        if(jQuery.fn.entry){ //Check if entry js file is loaded.
-            direction = $(this).entry({ e: event }); // Get mouse in direction.
-        }
-
-        $(this).removeClass(classNames.join(" ")); // Remove existing animation classes.
-        $(this).addClass("in-" + direction); //Add mouse in animation
-
-    }, 
+  });
     
-    function (event) {
-
-        var direction = "up";
-        if(jQuery.fn.entry){
-            direction = $(this).entry({ e: event }); // Get mouse out direction.
-        }
-
-        $(this).removeClass(classNames.join(" "));
-        $(this).addClass("out-" + direction); //Add mouse out animation
-
-    }
-);
+  $(".back-to-top").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+      $(".back-to-top").fadeOut("slow");
+     return false;
+  });
+    
+})(jQuery);
